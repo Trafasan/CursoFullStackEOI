de8901934961bd5a66f4d0b04eb13716c4e7e7c4
Sandra <sandrarequena1999@gmail.com>
2023-01-19 15:33:51 +0100

    Ejercicios de clase

--- ejerciciosEstructurasDeControl/src/ejerciciosEstructurasDeControl/Ejercicios11_15.java
+++ ejerciciosEstructurasDeControl/src/ejerciciosEstructurasDeControl/Ejercicios11_15.java
@@ -5,8 +5,6 @@ package ejerciciosEstructurasDeControl;
 
 import java.util.Scanner;
 
-import javax.swing.JOptionPane;
-
 /**
  * @author Sandra
  *
@@ -24,13 +22,57 @@ public class Ejercicios11_15 {
 		sc.nextLine();
 		System.out.print("Inserte el primer número: ");
 		double n2 = Double.parseDouble(sc.next());
+		System.out.println("1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir");
+		System.out.print("Inserte el número de la opereación que quiera realizar: ");
+		int op = sc.nextInt();
+		switch(op) {
+		case 1:
+			double suma = n1+n2;
+			System.out.println(n1+" + "+n2+" = "+suma);
+			break;
+		case 2:
+			double resta = n1-n2;
+			System.out.println(n1+" - "+n2+" = "+resta);
+			break;
+		case 3:
+			double mult = n1*n2;
+			System.out.println(n1+" · "+n2+" = "+mult);
+			break;
+		case 4:
+			if (n2 == 0) System.out.println("El divisor no puede ser 0.");
+			else {
+				double div = n1+n2;
+				System.out.println(n1+" / "+n2+" = "+div);
+			}
+			break;
+			default:
+				System.out.println("No se reconoció la opción seleccionada");
+		}
 		sc.nextLine();
 		sc.close();
 	}
 
 	public static void ejercicio12() {
-		
+		/*
+		 * Crea una variable con el precio de una entrada que inicialmente valga 50. Pregunta al usuario su edad.
+		 * a. Si es menor de edad se le hará un descuento del 25% en la entrada.
+		 * b. Si es mayor de edad pero no está jubilado (65 años), pregúntale si es socio. Si el usuario responde “sí” hazle un descuento del 40%.
+		 * c. Si está jubilado se le hará un descuento del 75%.
+		 * d. Finalmente muéstrale el precio que tiene que pagar por la entrada
+		 */
 		Scanner sc = new Scanner(System.in);
+		double precio = 50;
+		System.out.print("Introduzca su edad: ");
+		int edad = sc.nextInt();
+		if (edad>=0 && edad<18) precio *= (1-0.25);
+		else if (edad<65) {
+			System.out.println("¿Es usted socio? Responda con Sí o No");
+			String respuesta = sc.next();
+			precio = respuesta.equals("Sí")?precio*(1-0.4):precio;
+		}
+		else if (edad>=65) precio*=(1-0.75);
+		else System.out.println("La edad introducida es incorrecta.");
+		System.out.println("El precio de su entrada es de "+precio+"€");
 		sc.close();
 	}
 
@@ -54,7 +96,7 @@ public class Ejercicios11_15 {
 
 	public static void main(String[] args) {
 		// ejercicio11();
-		// ejercicio12();
+		ejercicio12();
 		// ejercicio13();
 		// ejercicio14();
 		// ejercicio15();
